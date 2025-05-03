26dd2a39dd7cfab35f8552358368604cc77711d2
Anish Mariathasan <anishmariathasan@gmail.com>
2025-05-03 15:31:41 +0100

    font and sidebar improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx}',
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        cream: '#F5F5DC',
+        black: '#000000',
+      },
+    },
+  },
   plugins: [],
 };
